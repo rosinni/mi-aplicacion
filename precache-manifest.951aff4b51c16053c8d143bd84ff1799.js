5ffe95d327e3475b23790e99a21587537a566762
Rosinni Rodriguez <ros.rm169@gmail.com>
2020-07-26 19:19:21 -0300

    Updates

---
+++ precache-manifest.951aff4b51c16053c8d143bd84ff1799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e514500ee72fef23411fe5c379a4d9ac",
+    "url": "/mi-aplicacion/index.html"
+  },
+  {
+    "revision": "d57bb5be201f3c0b5058",
+    "url": "/mi-aplicacion/static/css/main.aa93445d.chunk.css"
+  },
+  {
+    "revision": "5177617b820136132c27",
+    "url": "/mi-aplicacion/static/js/2.a42b0358.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mi-aplicacion/static/js/2.a42b0358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57bb5be201f3c0b5058",
+    "url": "/mi-aplicacion/static/js/main.53c68157.chunk.js"
+  },
+  {
+    "revision": "cffddac17811ef7b7242",
+    "url": "/mi-aplicacion/static/js/runtime-main.560d4c9b.js"
+  }
+]);
